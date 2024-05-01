2fd046c34daf6e77f38cf673cc9c18788502f231
JoseManuelFrancoAlzate <josemanuelalzatefranco12@gmail.com>
2024-04-30 20:42:10 -0500

    deploy

--- buenos-granizados/src/App.js
+++ buenos-granizados/src/App.js
@@ -7,7 +7,7 @@ import Configuracion from './Components/Configuracion/Configuracion';
 import {getActionAdmin} from './Redux/actions'
 import {useDispatch, useSelector} from 'react-redux'
 import { useEffect } from 'react';
-axios.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = "https://granizadosback.onrender.com/";
 
 
 function App() {
